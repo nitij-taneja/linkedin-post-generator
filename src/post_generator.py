55c1496e4206c3ebbc3ae845c610b2126d3e4b52
Nitij Taneja <124418733+nitij-taneja@users.noreply.github.com>
2025-05-22 20:01:31 +0530

    Update post_generator.py

--- src/post_generator.py
+++ src/post_generator.py
@@ -16,8 +16,9 @@ import base64
 
 logging.basicConfig(
     level=logging.INFO,
-    format=\'%(asctime)s - %(name)s - %(levelname)s - %(message)s\'
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
+
 logger = logging.getLogger(__name__)
 
 class PostGenerator:
